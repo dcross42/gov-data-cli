c72f42ed7c68f3cc8e5cbf4558e54f820644bd08
Daniel Crosson <40742799+dcross42@users.noreply.github.com>
2024-03-19 19:10:26 -0400

    First commit from Typer Documentation

---
+++ gov_data_cli/main.py
@@ -0,0 +1,23 @@
+import typer
+
+app = typer.Typer()
+
+@app.callback()
+def callback():
+    """
+    Example Code
+    """
+
+@app.command()
+def foo():
+    """
+    First command
+    """
+    typer.echo("Echoing First Command")
+
+@app.command()
+def bar():
+    """
+    Second Command
+    """
+    typer.echo("Echoing Second Command")
